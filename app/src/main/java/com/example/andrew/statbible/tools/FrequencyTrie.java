96f6ea2a8bf2021fb65f1623bc67492fd6dbc23c
Andrew Shubin <andrewbshubin@gmail.com>
2017-11-10 15:03:42 -0800

    Finished FTNode private class.

--- app/src/main/java/com/example/andrew/statbible/tools/FrequencyTrie.java
+++ app/src/main/java/com/example/andrew/statbible/tools/FrequencyTrie.java
@@ -12,7 +12,7 @@ public class FrequencyTrie {
 
     public FrequencyTrie(String source) {
         root = new FTNode();
-        for (String word : source.split(" ")) {
+        for (String word : source.toLowerCase().split(" ")) {
             this.add(word);
         }
     }
@@ -29,7 +29,11 @@ public class FrequencyTrie {
     }
 
     private void add(String word) {
-        // TODO
+        char[] letters = word.toCharArray();
+        FTNode current = root;
+        for (char letter : letters) {
+            current = current.add(letter);
+        }
     }
 
     private class FTNode {
@@ -45,6 +49,11 @@ public class FrequencyTrie {
             count = 0;
         }
 
+        public FTNode(char c) {
+            letter = c;
+            count = 1;
+        }
+
         /*
          * Method adds character c as a child of this node if a child
          * node does not already exist for character c. If the later is
@@ -53,17 +62,78 @@ public class FrequencyTrie {
          * Method returns the new (or incremented) node.
          */
         public FTNode add(char c) {
-
+            if (!Character.isLetter(c)) return this; // i.e. nothing happens.
+            if (child == null) {
+                child = new FTNode(c);
+                return child;
+            }
+            FTNode result = this.search(child, c);
+            if (result == null) {
+                result = new FTNode(c);
+                // Now insert it into the Trie...
+                result.next = child;
+                child = result;
+            } else {
+                result.count++;
+            }
+            return result;
         }
 
         /*
          * Method adds character c as the neighbor of this node in the same
          * manner and with the same stipulations as the add(char c) method.
+         *
+         * I'm not sure if I'll need this...implemented it just in case. For
+         * now, I'm making it private so I don't use it by accident if I
+         * don't need to.
          */
-        public FTNode stack(char c) {
-
+        private FTNode stack(char c) {
+            if (!Character.isLetter(c)) return this; // i.e. nothing happens.
+            FTNode result = this.search(this, c);
+            if (result == null) {
+                result = new FTNode(c);
+                if (next == null) {
+                    next = result;
+                } else {
+                    result.next = next;
+                    next = result;
+                }
+            } else {
+                result.count++;
+            }
+            return result;
         }
 
-        // TODO
+        /*
+         * Searches immediate children for node with character c.
+         *
+         * Returns node if found, returns null otherwise.
+         */
+        public FTNode find(char c) {
+            if (child == null) return null;
+            return this.search(child, c);
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        public char getLetter() {
+            return letter;
+        }
+
+        /*
+         * Searches neighbors starting with 'start' for 'letter'.
+         */
+        private FTNode search(FTNode start, char letter) {
+            FTNode current = start;
+            while (current != null) {
+                if (current.letter == letter) {
+                    return current;
+                }
+                current = current.next;
+            }
+            return null; // If matching node not found.
+        }
     }
 }
