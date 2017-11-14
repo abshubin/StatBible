35633e15d21ad3ceca70019e8922eb1cf6926810
ABShubin <abshubin@users.noreply.github.com>
2017-11-14 14:46:58 -0800

    Updated the header comment of FrequencyTrieUnitTest.java

--- app/src/test/java/com/example/andrew/statbible/FrequencyTrieUnitTest.java
+++ app/src/test/java/com/example/andrew/statbible/FrequencyTrieUnitTest.java
@@ -7,9 +7,8 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * Unit testing for the FrequencyTrie, to make sure it constructs
+ * and navigates the trie correctly.
  */
 public class FrequencyTrieUnitTest {
 
